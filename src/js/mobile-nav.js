56446dbe69125b83628aa8b4ce660ba42e9aa3d0
Brian Maier Jr <brimaidesigns@gmail.com>
2022-07-04 12:37:44 -0400

    + accessibility fixes

--- src/js/mobile-nav.js
+++ src/js/mobile-nav.js
@@ -1,11 +1,32 @@
 const menuTrigger = document.querySelector('.siteHeader__mobileNavTrigger');
 const menuCloseTrigger = document.querySelector('.mobileNav__close');
-const mobileNav = document.querySelector('.mobileNav');
+const mobileNav = document.querySelector('#mobileNav');
 
-menuTrigger.addEventListener('click', triggerMenu)
-menuCloseTrigger.addEventListener('click', triggerMenu)
+menuTrigger.addEventListener('click', openMenu)
+menuCloseTrigger.addEventListener('click', closeMenu)
 
-function triggerMenu() {
-  mobileNav.classList.toggle('active');
-  document.querySelector('body').classList.toggle('noScroll');
+function openMenu() {
+  menuTrigger.setAttribute('aria-expanded', 'true');
+  document.querySelector('body').classList.add('noScroll');
 }
+
+function closeMenu() {
+  menuTrigger.setAttribute('aria-expanded', 'false');
+  document.querySelector('body').classList.remove('noScroll');
+}
+
+const container = document.getElementById('mobileNav');
+
+const trap = focusTrap.createFocusTrap(container, {
+  onActivate: () => container.classList.add('active'),
+  onDeactivate: () => container.classList.remove('active'),
+  fallbackFocus: container,
+});
+
+document
+  .getElementById('menuTrigger')
+  .addEventListener('click', trap.activate);
+
+document
+  .getElementById('menuTriggerClose')
+  .addEventListener('click', trap.deactivate);
