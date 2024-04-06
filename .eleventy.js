9b7248bf963eb8b25bc24a8710095936e5da03a7
Brian Maier <brimaidesigns@gmail.com>
2024-04-06 13:25:39 -0400

    + sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,52 +1,47 @@
 const { Liquid } = require("liquidjs");
-
 const Image = require("@11ty/eleventy-img");
 
 async function imageShortcode(src, alt, sizes) {
-  let metadata = await Image(src, {
-    widths: [300, 600],
-    formats: ["avif", "jpeg"]
-  });
+	let metadata = await Image(src, {
+		widths: [300, 600],
+		formats: ["avif", "jpeg"],
+	});
 
-  let imageAttributes = {
-    alt,
-    sizes,
-    loading: "lazy",
-    decoding: "async",
-  };
+	let imageAttributes = {
+		alt,
+		sizes,
+		loading: "lazy",
+		decoding: "async",
+	};
 
-  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-  return Image.generateHTML(metadata, imageAttributes);
+	// You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+	return Image.generateHTML(metadata, imageAttributes);
 }
 
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addWatchTarget("src/sass/");
+	eleventyConfig.addPassthroughCopy({ "src/img": "assets/img" });
+	eleventyConfig.addPassthroughCopy({ "src/fonts": "assets/fonts" });
+	eleventyConfig.addPassthroughCopy({ "src/js": "assets/js" });
+	eleventyConfig.addPassthroughCopy({ "src/favicon": "./" });
+	eleventyConfig.addLiquidShortcode("image", imageShortcode);
 
-  eleventyConfig.addWatchTarget('src/sass/');
-  eleventyConfig.addPassthroughCopy({ 'src/img': 'assets/img' });
-  eleventyConfig.addPassthroughCopy({ 'src/fonts': 'assets/fonts' });
-  eleventyConfig.addPassthroughCopy({ 'src/js': 'assets/js' });
-  eleventyConfig.addPassthroughCopy({ 'src/favicon': './' });
-  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+	// eleventyConfig.setBrowserSyncConfig({
+	//   files: ['_site/assets/css/*.css']
+	// });
 
-  // eleventyConfig.setBrowserSyncConfig({
-  //   files: ['_site/assets/css/*.css']
-  // });
+	return {
+		dir: {
+			// ⚠️ These values are both relative to your input directory.
+			input: "src",
+			output: "_site",
+			includes: "_includes",
+			layouts: "_layouts",
+		},
+	};
 
-  return {
-    dir: {
-      // ⚠️ These values are both relative to your input directory.
-      input: "src",
-      output: "_site",
-      includes: "_includes",
-      layouts: "_layouts"
-    }
-  };
-
-  eleventyConfig.setLiquidOptions({
-    dynamicPartials: true,
-    strictFilters: true, // renamed from `strict_filters` in Eleventy 1.0
-  });
-
+	eleventyConfig.setLiquidOptions({
+		dynamicPartials: true,
+		strictFilters: true, // renamed from `strict_filters` in Eleventy 1.0
+	});
 };
-
-
